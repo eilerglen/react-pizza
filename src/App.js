2925e534c486f3816e2fd93302877069e7cd26da
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-08-01 18:13:56 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./scss/app.scss";
+import { Header } from "./components/header";
+import { Categories } from "./components/categories";
+import { Sort } from "./components/sort";
+import { PizzaItem } from "./components/pizza-item";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaItem title = "Мексиканская" price={500}/>
+            <PizzaItem title = "Мексиканская" price={350}/>
+            <PizzaItem title = "Мексиканская" price={450}/>
+            <PizzaItem title = "Мексиканская" price={600}/>
+            <PizzaItem title = "Мексиканская" price={250}/>
+            <PizzaItem title = "Мексиканская" price={390}/>
+            <PizzaItem title = "Мексиканская" price={400}/>
+            <PizzaItem title = "Мексиканская" price={400}/>
+            <PizzaItem title = "Мексиканская" price={350}/>
+            <PizzaItem title = "Мексиканская" price={550}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
